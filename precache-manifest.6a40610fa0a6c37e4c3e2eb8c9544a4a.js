862e906947ae6fb847ca188873480bc12bf6c5af
sanyalam <sandeep.yalamarthi@gmail.com>
2019-07-01 23:59:31 +0530

    Updates

---
+++ precache-manifest.6a40610fa0a6c37e4c3e2eb8c9544a4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6035da96ad2d4a35afb126307d8d6ab",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "c4d7496c8dd5c43ad2ab",
+    "url": "/myresume/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/myresume/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c4d7496c8dd5c43ad2ab",
+    "url": "/myresume/static/js/main.9df74994.chunk.js"
+  },
+  {
+    "revision": "95e7c0f5a6680da6caf6",
+    "url": "/myresume/static/js/runtime~main.6f2b030a.js"
+  }
+]);
